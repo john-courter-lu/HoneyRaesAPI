2259e9388a5a7004901bdb6bda75200ca1c83e42
John Lu <johnlu2006@gmail.com>
2023-08-23 15:50:55 -0500

    Adding data to the collections using the collection initializers.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,32 @@
+using Microsoft.AspNetCore.Http.Json;
+using System.Text.Json.Serialization;
+using HoneyRaesAPI.Models;
+
+List<Customer> customers = new List<Customer>
+    {
+        new Customer { Id = 1, Name = "John Doe", Address = "123 Main St" },
+        new Customer { Id = 2, Name = "Jane Smith", Address = "456 Elm St" },
+        new Customer { Id = 3, Name = "Robert Johnson", Address = "789 Oak St" }
+    };
+
+List<Employee> employees = new List<Employee>
+    {
+        new Employee { Id = 1, Name = "Alice Brown", Specialty = "Plumber" },
+        new Employee { Id = 2, Name = "Bob Green", Specialty = "Electrician" }
+    };
+
+List<ServiceTicket> serviceTickets = new List<ServiceTicket>
+    {
+        new ServiceTicket { Id = 1, CustomerId = 1, EmployeeId = 1, Description = "Leaky faucet repair", Emergency = false, DateCompleted = DateTime.Now.AddDays(-1) },
+        new ServiceTicket { Id = 2, CustomerId = 2, EmployeeId = 1, Description = "Power outage troubleshooting", Emergency = true, DateCompleted = DateTime.Now.AddDays(-2) },
+        new ServiceTicket { Id = 3, CustomerId = 3, EmployeeId = 2, Description = "Wiring installation", Emergency = false, DateCompleted = DateTime.Now.AddDays(-3) },
+        new ServiceTicket { Id = 4, CustomerId = 1, EmployeeId = 2, Description = "Circuit breaker replacement", Emergency = true, DateCompleted = DateTime.Now.AddDays(-4) },
+        new ServiceTicket { Id = 5, CustomerId = 2, EmployeeId = 1, Description = "Sink drain cleaning", Emergency = false },
+        new ServiceTicket { Id = 6, CustomerId = 3, EmployeeId = 2, Description = "Light fixture installation", Emergency = false },
+        new ServiceTicket { Id = 7, CustomerId = 1, EmployeeId = 1, Description = "Toilet repair", Emergency = true },
+        new ServiceTicket { Id = 8, CustomerId = 2, Description = "Outlet replacement", Emergency = false }
+    };
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Set the JSON serializer options
@@ -37,7 +66,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateTime.Now.AddDays(index),
