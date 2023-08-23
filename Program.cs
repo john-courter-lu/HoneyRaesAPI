d85639bdc4d9b54799efe85d499e201b2004f21a
John Lu <johnlu2006@gmail.com>
2023-08-23 15:23:54 -0500

    Finish the type definitions for our data models

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Set the JSON serializer options
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
