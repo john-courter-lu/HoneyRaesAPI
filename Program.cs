4aa70af3762ecda508696a2be72d6793afc44ddc
John Lu <johnlu2006@gmail.com>
2023-08-24 15:39:21 -0500

    mapPost to mark as completed

--- Program.cs
+++ Program.cs
@@ -157,6 +157,14 @@ app.MapPut("/servicetickets/{id}", (int id, ServiceTicket serviceTicket) =>
     return Results.Ok();
 });
 
+app.MapPost("/servicetickets/{id}/complete", (int id) =>
+{
+
+ServiceTicket ticketToComplete = serviceTickets.FirstOrDefault(st => st.Id == id);
+ticketToComplete.DateCompleted = DateTime.Today;
+
+});
+
 app.Run();
 // 这里是run this app
 
